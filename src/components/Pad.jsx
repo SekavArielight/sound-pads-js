dc3bb0c5e2075dc38c69c432073be2ad343258f3
God'swill Sekav Apya <godswillapya@gmail.com>
2025-07-11 16:59:14 +0100

    Remove challenge comments from App and Pad components for clarity

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -2,13 +2,6 @@ const Pad = ({color, on}) => {
 
   return (
 
-    /**
-     * Challenge part 3:
-     * Our buttons got turned off by default! Update the code
-     * so if the button is "on", it has the className of "on".
-     */
-
-
     <button style={{backgroundColor: color}} className={on ? "on" : ""}></button>
   )
 }
